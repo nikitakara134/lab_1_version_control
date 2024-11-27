f64ea36c9ffa1fcd90895cd4f80f8f679939b940
nikitakara134 <127400122+nikitakara134@users.noreply.github.com>
2024-11-27 15:24:55 +0200

    Update 2.js

--- 2.js
+++ 2.js
@@ -4,7 +4,13 @@ const updateTime = () => {
     const timeZone = document.getElementById('timezone').value;
     const options = { timeZone, hour: '2-digit', minute: '2-digit', second: '2-digit' };
     const formattedTime = currentTime.toLocaleTimeString('uk-UA', options);
-    document.getElementById('current-time').textContent = `Поточний час: ${formattedTime}`;
+    
+    // Викликаємо помилку з неправильним іменем змінної
+    if (!document.getElementById('current-Tim')) {
+        throw new Error('currentTim is not defined'); // Генеруємо помилку
+    }
+    
+    document.getElementById('current-Tim').textContent = `Поточний час: ${formattedTime}`;
 };
 
 module.exports = { updateTime };
