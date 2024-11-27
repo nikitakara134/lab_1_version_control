0092a6c6817391783dadb3890f066fd43734e9a0
nikitakara134 <127400122+nikitakara134@users.noreply.github.com>
2024-11-27 15:14:14 +0200

    Update 2.test.js

--- 2.test.js
+++ 2.test.js
@@ -1,5 +1,4 @@
 // 2.test.js
-
 const fs = require('fs');
 const path = require('path');
 
@@ -8,6 +7,9 @@ const html = fs.readFileSync(path.resolve(__dirname, '1.html'), 'utf8');
 
 // Мокаємо документ в jsdom
 global.document = new (require('jsdom')).JSDOM(html).window.document;
+
+// Підключаємо функцію, яка викликає помилку в updateTime (це має бути частина вашого 2.js)
+global.updateTime = require('./2.js').updateTime;
 
 describe('Time zone app', () => {
     it('should select the correct timezone', () => {
@@ -18,5 +20,10 @@ describe('Time zone app', () => {
     it('should update the time display', () => {
         const currentTimeDisplay = document.getElementById('current-time');
         expect(currentTimeDisplay).not.toBeNull(); // Перевірка, чи елемент існує
+    });
+
+    it('should fail when updateTime function has an error', () => {
+        // Перевіряємо, чи функція updateTime викликає помилку
+        expect(() => updateTime()).toThrowError('currentTim is not defined'); // Тут вказуємо помилку, яку ми очікуємо
     });
 });
